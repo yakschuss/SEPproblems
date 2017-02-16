b4b41a6d5ee56e0eadb68c2824e12bb7e1a9b1e2
Jack Schuss <jackschuss@gmail.com>
2017-02-15 21:07:57 -0500

    Line Assignment

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,27 @@ class Line
   end
 
   def join(person)
+    members.push(person)
   end
 
   def leave(person)
+    members.delete_if {|p| p == person}
   end
 
   def front
+    members[0]
   end
 
   def middle
+    members[members.length / 2]
   end
 
   def back
+    members[members.length - 1]
   end
 
   def search(person)
-  end
-
-  private
-
-  def index(person)
+    members.select {|p| p == person}[0]
   end
 
 end
